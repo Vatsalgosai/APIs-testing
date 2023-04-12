5da8373d2ade8421f3353928dc271f389890b8a0
Vatsal Gosai <vatsl.g@solutelabs.com>
2023-04-12 09:27:06 +0530

    testing

---
+++ test/feed-controller.js
@@ -0,0 +1,68 @@
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const mongoose = require("mongoose");
+
+const User = require("../models/user");
+const FeedController = require("../controllers/feed");
+
+describe("Feed Controller", function () {
+  before(function (done) {
+    mongoose
+      .connect(
+        "mongodb+srv://vatsal:XcU-54K$t386tz2@cluster0.znpgm81.mongodb.net/test-messages",
+        { useNewUrlParser: true }
+      )
+      .then((result) => {
+        const user = new User({
+          email: "vatsal@test.com",
+          password: "vatsal12",
+          name: "vatsal",
+          posts: [],
+          _id: "642d76495a1aa747dc1afcea",
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  beforeEach(function () {});
+
+  afterEach(function () {});
+
+  it("should add a created post to the posts of the creator", function (done) {
+    const req = {
+      body: {
+        title: "Test Post",
+        content: "A Test Post",
+      },
+      file: {
+        path: "abc",
+      },
+      userId: "642d76495a1aa747dc1afcea",
+    };
+    const res = {
+      status: function () {
+        return this;
+      },
+      json: function () {},
+    };
+
+    FeedController.createPost(req, res, () => {}).then((savedUser) => {
+      expect(savedUser).to.have.property("posts");
+      expect(savedUser.posts).to.have.length(1);
+      done();
+    });
+  });
+
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
